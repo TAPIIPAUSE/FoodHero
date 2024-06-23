fe515b7770a061a3e69b357205e80bc70e363cd3
Pongpat <pause.pongpat@gmail.com>
2024-06-23 23:18:39 +0700

    colors theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,64 +3,18 @@ import 'package:flutter/material.dart';
 class AppTheme {
   AppTheme._();
   static const Color greenMainTheme = Color.fromRGBO(67, 189, 174, 100);
-  static const Color greenMainThme = Color.fromRGBO(197, 255, 245, 100);
-
-  static ThemeData greenBackground = ThemeData(
-    //primaryColor:Color. fromRGBO(197, 255, 245, 100%),
-    primaryColor: const Color.fromRGBO(197, 255, 245, 100),
-  );
-
-  static ThemeData softBrightGreen = ThemeData(
-    primaryColor: const Color.fromRGBO(137, 223, 172, 100),
-  );
-
-  static ThemeData softGreen = ThemeData(
-    primaryColor: const Color.fromRGBO(202, 255, 201, 100),
-  );
-
-  static ThemeData lightGreen = ThemeData(
-    primaryColor: const Color.fromRGBO(202, 255, 201, 100),
-  );
-
-  static ThemeData mainBlue = ThemeData(
-    primaryColor: const Color.fromRGBO(54, 103, 187, 100),
-  );
-
-  static ThemeData softBlue = ThemeData(
-    primaryColor: const Color.fromRGBO(169, 209, 255, 100),
-  );
-
-  static ThemeData orangeGray = ThemeData(
-    primaryColor: const Color.fromRGBO(159, 141, 134, 100),
-  );
-
-  static ThemeData softRedCancleWasted = ThemeData(
-    primaryColor: const Color.fromRGBO(255, 134, 134, 100),
-  );
-
-  static ThemeData spoiledBrown = ThemeData(
-    primaryColor: const Color.fromRGBO(237, 168, 138, 100),
-  );
-
-  static ThemeData softOrange = ThemeData(
-    primaryColor: const Color.fromRGBO(255, 197, 110, 100),
-  );
-
-  static ThemeData softRed = ThemeData(
-    primaryColor: const Color.fromRGBO(243, 203, 186, 100),
-  );
-
-  static ThemeData softYellow = ThemeData(
-    primaryColor: const Color.fromRGBO(255, 246, 163, 100),
-  );
-
-  static ThemeData softRedBrown = ThemeData(
-    primaryColor: Color.fromRGBO(241, 228, 221, 0.612),
-  );
-
-  //Text
-  static final TextTheme textTheme = TextTheme(
-    //headline1: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, fontFamily: 'Mouse')
-  );
-  
+  static const Color softGreenMainThme = Color.fromRGBO(197, 255, 245, 100);
+  static const Color lightGreenBackground = Color.fromRGBO(197, 255, 245, 100);
+  static const Color softGreen = Color.fromRGBO(202, 255, 201, 100);
+  static const Color softBlue = Color.fromRGBO(169, 209, 255, 100);
+  static const Color softBrightGreen = Color.fromARGB(137, 223, 172, 100);
+  static const Color lightGreen = Color.fromARGB(202, 255, 201, 100);
+  static const Color mainBlue = Color.fromRGBO(54, 103, 187, 100);
+  static const Color orangeGray = Color.fromRGBO(159, 141, 134, 100);
+  static const Color softRedCancleWasted = Color.fromRGBO(255, 134, 134, 1.0);
+  static const Color spoiledBrown = Color.fromRGBO(237, 168, 138, 1.0);
+  static const Color softOrange = Color.fromRGBO(255, 197, 110, 1.0);
+  static const Color softRed = Color.fromRGBO(243, 203, 186, 1.0);
+  static const Color softYellow = Color.fromRGBO(255, 246, 163, 1.0);
+  static const Color softRedBrown = Color.fromRGBO(241, 228, 221, 0.612);
 }
