6449de292c6c71db5c88448a68ddcc045e66aef5
Pongpat <pause.pongpat@gmail.com>
2024-06-24 21:17:22 +0700

    fonts theme

--- lib/fonts.dart
+++ lib/fonts.dart
@@ -1,13 +1,139 @@
 import 'package:flutter/material.dart';
-import 'package:foodhero/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class fonts extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
+class FontsTheme {
+  
+  static TextStyle mouseMemoirs_64({
+    FontWeight fontWeight = FontWeight.w400,
+    double fontSize = 64,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.mouseMemoirs(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
 
-    return Scaffold(
-      appBar: AppBar(),
+  static TextStyle mouseMemoirs_50({
+    FontWeight fontWeight = FontWeight.w400,
+    double fontSize = 50,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.mouseMemoirs(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle mouseMemoirs_40({
+    FontWeight fontWeight = FontWeight.w400,
+    double fontSize = 40,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.mouseMemoirs(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle mouseMemoir_30({
+    FontWeight fontWeight = FontWeight.w400,
+    double fontSize = 30,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.mouseMemoirs(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle mouseMemoirs_20({
+    FontWeight fontWeight = FontWeight.w400,
+    double fontSize = 20,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.mouseMemoirs(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+
+
+  static TextStyle hindBold_32({
+    FontWeight fontWeight = FontWeight.w700,
+    double fontSize = 32,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.hind(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle hindBold_30({
+    FontWeight fontWeight = FontWeight.w700,
+    double fontSize = 30,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.hind(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle hindBold_20({
+    FontWeight fontWeight = FontWeight.w700,
+    double fontSize = 20,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.hind(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle hind_20({
+    FontWeight fontWeight = FontWeight.w400,
+    double fontSize = 20,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.hind(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle hindBold_15({
+    FontWeight fontWeight = FontWeight.w700,
+    double fontSize = 15,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.hind(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle hind_15({
+    FontWeight fontWeight = FontWeight.w400,
+    double fontSize = 15,
+    Color color = const Color(0xFF000000),
+  }) {
+    return GoogleFonts.hind(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
     );
   }
 }
