3ad85a0768f8b7e35169b76864d264848b65ec68
Pongpat <pause.pongpat@gmail.com>
2024-06-30 12:42:46 +0700

    Updated theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,19 +2,17 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   AppTheme._();
-  static const Color greenMainTheme = Color.fromRGBO(67, 189, 174, 100);
-  static const Color softGreenMainThme = Color.fromRGBO(197, 255, 245, 100);
-  static const Color lightGreenBackground = Color.fromRGBO(197, 255, 245, 100);
-  static const Color softGreen = Color.fromRGBO(202, 255, 201, 100);
-  static const Color softBlue = Color.fromRGBO(169, 209, 255, 100);
-  static const Color softBrightGreen = Color.fromARGB(137, 223, 172, 100);
-  static const Color lightGreen = Color.fromARGB(202, 255, 201, 100);
-  static const Color mainBlue = Color.fromRGBO(54, 103, 187, 100);
-  static const Color orangeGray = Color.fromRGBO(159, 141, 134, 100);
+  static const Color greenMainTheme = Color.fromRGBO(67, 189, 174, 1.0);
+  static const Color lightGreenBackground = Color.fromRGBO(197, 255, 245, 1.0);
+  static const Color softGreen = Color.fromRGBO(202, 255, 201, 1.0);
+  static const Color softBlue = Color.fromRGBO(169, 209, 255, 1.0);
+  static const Color softBrightGreen = Color.fromRGBO(137, 223, 172, 1.0);
+  static const Color mainBlue = Color.fromRGBO(54, 103, 187, 1.0);
+  static const Color orangeGray = Color.fromRGBO(159, 141, 134, 1.0);
   static const Color softRedCancleWasted = Color.fromRGBO(255, 134, 134, 1.0);
   static const Color spoiledBrown = Color.fromRGBO(237, 168, 138, 1.0);
   static const Color softOrange = Color.fromRGBO(255, 197, 110, 1.0);
   static const Color softRed = Color.fromRGBO(243, 203, 186, 1.0);
   static const Color softYellow = Color.fromRGBO(255, 246, 163, 1.0);
-  static const Color softRedBrown = Color.fromRGBO(241, 228, 221, 0.612);
+  static const Color softRedBrown = Color.fromRGBO(241, 228, 221, 1.0);
 }
