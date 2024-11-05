f4eb2ade879a980c28aa38ae723b9c7fa669b200
Supasek Dhanabordeephat <supasekmai@gmail.com>
2024-11-05 14:48:12 +0700

    Create get name for house and organization

--- routes/houseorg.js
+++ routes/houseorg.js
@@ -3,6 +3,8 @@ import { get_user_from_db } from "../service/user_service.js";
 import { preprocess_House_Score, preprocess_Org_Score } from "../service/score_service.js";
 import { authenticateToken } from "../service/jwt_auth.js";
 import { preprocess_house_barchart, preprocess_org_barchart} from "../service/houseorg_service.js";
+import House from "../schema/user_module/houseSchema.js";
+import Organization from "../schema/user_module/organizationSchema.js";
 
 const router = express.Router();
 
@@ -88,5 +90,40 @@ router.get("/organization/weekly_fs-bar", authenticateToken, async (req, res) =>
   }
 });
 
+router.get("/household/getHouseName", authenticateToken, async (req, res) => {
+
+
+  try {
+    var user = await get_user_from_db(req, res);
+    // We get Score Array from this Function
+    var house = await House.findOne({assigned_ID: user.hID})
+
+    console.log(house)
+    return res.status(200).send({
+      "Messages": "Successfully Get House Name",
+      "House_Name": house.house_name
+    });
+  } catch (error) {
+    return res.status(400).send(`Error when retrieving household score: ${error}`);
+  }
+});
+
+router.get("/organization/getOrgName", authenticateToken, async (req, res) => {
+
+
+  try {
+    var user = await get_user_from_db(req, res);
+    // We get Score Array from this Function
+    var organization = await Organization.findOne({assigned_ID: user.orgID})
+
+    
+    return res.status(200).send({
+      "Messages": "Successfully Get Organization Name",
+      "Organiazation_Name": organization.org_name
+    });
+  } catch (error) {
+    return res.status(400).send(`Error when retrieving household score: ${error}`);
+  }
+});
 
 export default router;
